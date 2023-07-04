75881a01c31d9c55dc8a3930a0b4094a12fb2611
prakharsinghal311 <prakhargd31121999@gmail.com>
2023-07-04 20:50:15 +0530

    first commit

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Extreme_Makeover%2C_Mesa_Verde_Edition_-_panoramio.jpg/220px-Extreme_Makeover%2C_Mesa_Verde_Edition_-_panoramio.jpg"
+      title="First Meetup"
+      address="Some Street 5, Some City"
+      description="This is a first Meetup"
+    />
+  );
+}
+
+export default MeetupDetails;
---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Extreme_Makeover%2C_Mesa_Verde_Edition_-_panoramio.jpg/220px-Extreme_Makeover%2C_Mesa_Verde_Edition_-_panoramio.jpg",
+    address: "Some address 5, 12345 Some city",
+    description: "This is a first Meetup!",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Extreme_Makeover%2C_Mesa_Verde_Edition_-_panoramio.jpg/220px-Extreme_Makeover%2C_Mesa_Verde_Edition_-_panoramio.jpg",
+    address: "Some address 10, 12345 Some city",
+    description: "This is a second Meetup!",
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+
+export default HomePage;
