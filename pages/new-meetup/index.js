51c07ecd5783106e9a0b425f21ec3d0629a5fa35
prakharsinghal311 <prakhargd31121999@gmail.com>
2023-07-06 21:08:31 +0530

    SEO Friendly

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,4 +1,5 @@
 // our-domain.com/new-meetup
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
@@ -20,7 +21,18 @@ function NewMeetupPage() {
     router.push("/");
   }
 
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+  return (
+    <>
+      <Head>
+        <title>Add a new meetup</title>
+        <meta
+          name="description"
+          content="Add your own meetups and create amazing networking opportunities."
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+    </>
+  );
 }
 
 export default NewMeetupPage;
